b40233b6913cee4a65c7412f8a8cf6b6dfdc0132
Bishop-Y <dizzydw@yandex.ru>
2023-04-02 22:15:07 +0300

    Добавил хеширование паролей, соль и обновил main

--- Authorization_Registration_PasswordRecovery/functions.py
+++ Authorization_Registration_PasswordRecovery/functions.py
@@ -1,5 +1,8 @@
 import psycopg2
 from psycopg2 import Error
+import hashlib
+import secrets
+
 
 def create_connection(db_name, db_user, db_password, db_host, db_port):
     connection = None
@@ -17,26 +20,48 @@ def create_connection(db_name, db_user, db_password, db_host, db_port):
     return connection
 
 
-def authenticate(email_or_phone, password):
-    connection = create_connection(
-        'course_project_db', 'amepifanov', 'fhntv2003',  'localhost', '5050'
-    )
-    cursor = connection.cursor()
-    if (type(email_or_phone) is str):
-        cursor.execute(
-            f"SELECT * FROM users WHERE email = '{email_or_phone}' AND password = '{password}'")
+def hash_password(password, salt):
+    hash_object = hashlib.sha256()
+    hash_object.update((password + salt).encode('utf-8'))
+    hashed_password = hash_object.hexdigest()
+    return hashed_password
 
-    else:
-        cursor.execute(
-            f"SELECT * FROM users WHERE phone = {email_or_phone} AND password = '{password}'")
+
+def generate_salt():
+    return secrets.token_hex(16)
+
+
+def authenticate(email_or_phone, password, connection):
+    cursor = connection.cursor()
+    cursor.execute(
+        f"SELECT * FROM users WHERE email = '{email_or_phone}' OR phone = '{email_or_phone}'")
 
     user = cursor.fetchone()
-    print(user)
-    cursor.close()
-    connection.close()
     if user:
-        return True
+        hashed_password, salt = user[2], user[3]
+        if (hash_password(password, salt) == hashed_password):
+            return True
+        else:
+            print("\n" + 'Invalid password' + "\n")
+            return False
     else:
+        print('User with this email or password doesn\'t exists')
         return False
 
+
+def register(email, phone, password, connection):
+    cursor = connection.cursor()
+
+    cursor.execute(
+        f"SELECT * FROM users WHERE email='{email}' OR phone='{phone}'")
+    user = cursor.fetchone()
+    if user:
+        print("\n" + 'User with this email or phone already exist' + "\n")
+    else:
+        salt = generate_salt()
+        hashed_password = hash_password(password, salt)
+        cursor.execute(
+            f"INSERT INTO users VALUES('{email}', '{phone}', '{hashed_password}', '{salt}');")
+        connection.commit()
+        print("\n" + 'Succsess' + "\n")
 
--- Authorization_Registration_PasswordRecovery/main.py
+++ Authorization_Registration_PasswordRecovery/main.py
@@ -1,4 +1,6 @@
-from test import *
+from functions import create_connection
+from functions import authenticate
+from functions import register
 
 connection = create_connection(
     'course_project_db', 'amepifanov', 'fhntv2003',  'localhost', '5050'
@@ -8,20 +10,28 @@ cursor = connection.cursor()
 cursor.execute('''DROP TABLE users''')
 cursor.execute('''CREATE TABLE users(
     email VARCHAR(30) NOT NULL,
-    phone BIGINT NOT NULL,
-    password VARCHAR(30) NOT NULL
+    phone VARCHAR(11) NOT NULL,
+    hashed_password VARCHAR(64) NOT NULL,
+    salt VARCHAR(32) NOT NULL
 ); ''')
-cursor.execute(
-    '''INSERT INTO users VALUES('epifanov03@mail.ru', 89261091717, 'fhntv2003');
-    INSERT INTO users VALUES('dizzydw@yandex.ru', '89268955219', 'fhntv2003');''')
-connection.commit()
+
+register('epifanov03@mail.ru', '89261091717', '1234', connection)
+
+register('dizzydw@yandex.ru', '89268955219', '1234', connection)
+
+register('abcd@mail.ru', '89261091717', '1234', connection)
+
+register('dizzydw@yandex.ru', '89268955211', '1234', connection)
+
+register('abcd@mail.ru', '89268955211', '1234', connection)
 
 cursor.execute('SELECT * FROM public.users')
 record = cursor.fetchall()
+print("\n" + str(record) + "\n")
+
+
+print(authenticate('epifanov03@mail.ru', 'hui', connection))
+print('\n\n')
+print(authenticate('epifanov03@mail.ru', '1234', connection))
 cursor.close()
 connection.close()
-print(record)
-
-
-#print(authenticate(89261091717, 'fhntv2003'))
-print(authenticate('epifanov03@mail.ru', 'fhntv2003'))
