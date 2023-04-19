eaf7002e0cb75f685f87849b669cff5132150f8a
Alexander <asfrolov_2@edu.hse.ru>
2023-04-19 14:24:08 +0300

    1) Страница для вывода полученного токена заменена на страницу с подтверждением того, что бот успешно подключен и возможностью перейти на главную страницу (её пока нет) и на страницу по подключению тг бота
    2) страницы украшены css файлами
    
    3) логика работы программы улучшена

---
+++ Telegram/Functions.js
@@ -0,0 +1,7 @@
+// function displayError(message) {
+//     var tokenInput = document.getElementsByName("token")[0];
+//     var errorMessage = document.getElementById("error-message");
+//     tokenInput.classList.add("error");
+//     errorMessage.innerHTML = message;
+//     errorMessage.style.display = "block";
+// }
