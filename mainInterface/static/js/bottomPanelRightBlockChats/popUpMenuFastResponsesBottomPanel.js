fa18f5bcf241a29747b90ac4c5cb2a34d3f9cb91
Alexander <asfrolov_2@edu.hse.ru>
2023-05-02 13:52:37 +0300

    функция fetchFastResponses() теперь работает корректно и повторяет попытки загрузки до тех пор, пока загрузка не будет успешной

--- mainInterface/static/js/bottomPanelRightBlockChats/popUpMenuFastResponsesBottomPanel.js
+++ mainInterface/static/js/bottomPanelRightBlockChats/popUpMenuFastResponsesBottomPanel.js
@@ -17,25 +17,36 @@ function activateVisualPartFastResponses() {
     }
 }
 
-function fetchFastResponses() {
-    fetch('/fastResponses')
-        .then(response => response.json())
-        .then(fastResponses => {
-            // clear existing chat list content
-            popupContainer.innerText = '';
-            // create chat box for each chat item
-            fastResponses.forEach(response => {
-                const responseBox = document.createElement('li');
-                responseBox.classList.add('response')
-                responseBox.innerHTML = `
+async function fetchFastResponses() {
+    let response;
+    let success = false;
+
+    let fastResponses;
+    while (!success) {
+        try {
+            response = await fetch('/fastResponses');
+            fastResponses = await response.json();
+            success = true;
+        } catch (err) {
+            console.error('Error fetching chats. Retrying in 0.5 seconds...', err);
+            await new Promise(resolve => setTimeout(resolve, 500));
+        }
+    }
+
+    // clear existing chat list content
+    popupContainer.innerText = '';
+    // create chat box for each chat item
+    fastResponses.forEach(response => {
+        const responseBox = document.createElement('li');
+        responseBox.classList.add('response')
+        responseBox.innerHTML = `
                     <p class="short-version" id="short-version">${response.short_version}</p>
                     <p class="full-version" id="full-version"> ${response.full_version}</p>
                 `;
-                responseBox.addEventListener('click', () => {
-                    messageInputBox.value = responseBox.querySelector('.full-version').textContent;
-                    popupBlock.classList.remove('active');
-                })
-                popupContainer.appendChild(responseBox);
-            });
-        });
+        responseBox.addEventListener('click', () => {
+            messageInputBox.value = responseBox.querySelector('.full-version').textContent;
+            popupBlock.classList.remove('active');
+        })
+        popupContainer.appendChild(responseBox);
+    });
 }
